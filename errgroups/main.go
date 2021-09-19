6964918a4f65c8d1182ce2fce026ecb086608d3a
Evildoer <xuejingdan@laoyuegou.com>
2021-09-19 23:28:00 +0800

    errgroup

---
+++ errgroups/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+)
+
+func main() {
+	g, ctx := errgroup.WithContext(context.Background())
+	mu := http.NewServeMux()
+	server := &http.Server{
+		Addr:    "127.0.0.1:8000",
+		Handler: mu,
+	}
+	g.Go(func() error {
+		fmt.Println("http")
+		go func() {
+			<-ctx.Done()
+			fmt.Println("http ctx done")
+			server.Shutdown(context.TODO())
+		}()
+		return server.ListenAndServe()
+	})
+
+	g.Go(func() error {
+		exitSignals := []os.Signal{os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGINT}
+		sig := make(chan os.Signal, len(exitSignals))
+		signal.Notify(sig, exitSignals...)
+		for {
+			fmt.Println("signal")
+			select {
+			case <-ctx.Done():
+				fmt.Println("signal ctx done")
+				return ctx.Err()
+			case <-sig:
+				//do something
+				return nil
+			}
+
+		}
+	})
+
+	g.Go(func() error {
+		fmt.Println("inject")
+		time.Sleep(time.Second)
+		fmt.Println("inject finsh")
+		return errors.New("inject error")
+	})
+	err := g.Wait()
+	fmt.Println(err)
+}
